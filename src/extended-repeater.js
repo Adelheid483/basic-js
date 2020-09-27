2d4dc4087535157ab444791b56d7e5c4443f2d81
Adelheid483 <adelheid0106@gmail.com>
2020-09-27 19:46:49 +0300

    feat: add extended repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,12 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options
-    str = str.toString();
-    addition = addition.toString();
+  let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options;
 
-    let addPart = ''; 
-    let newStr = ''; 
+    str = String(str);
+    addition = String(addition);
+
+    let addPart = ''; // additional part of string
+    let newStr = ''; // result
         
     if (addition && separator) {
       if (additionSeparator) {
@@ -14,10 +15,10 @@ module.exports = function repeater(str, options) {
       } else {
           addPart = addition.repeat(additionRepeatTimes)
       }
-      newStr = str + addPart + separator
+      newStr = str + addPart + separator;
       return newStr.repeat(repeatTimes).slice(0, -separator.length)
     } else if(separator) {
-        newStr = str + separator
+        newStr = str + separator;
         return newStr.repeat(repeatTimes).slice(0, -separator.length)
     } else {
         return str.repeat(repeatTimes)
