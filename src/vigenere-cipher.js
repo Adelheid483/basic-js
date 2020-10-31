f0d6ef91517f97a19b3acfa4a1f5ba89209738c4
Adelheid483 <adelheid0106@gmail.com>
2020-10-31 17:06:42 +0300

    feat: start vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Arguments not found');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    while (key.length < message.length) {
+      key += key;
+    }
+    key = key.substr(0, message.length);
+
+    let result = [];
+    for (let i = 0; i < message.length; i++) {
+      let char = message.charCodeAt(i) + (key.charCodeAt(i) - 65);
+      if (char > 90) {
+        char = 64 + ((key.charCodeAt(i) - 65) - (90 - message.charCodeAt(i)));
+      }
+      result.push(String.fromCharCode(char));
+    }
+    return this.mode ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Arguments not found');
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
   }
 }
 
