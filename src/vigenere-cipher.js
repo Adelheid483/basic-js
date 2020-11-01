6fc6e02880840bb3c9a788d1bcbb9161ee1287f0
Adelheid483 <adelheid0106@gmail.com>
2020-11-01 19:23:49 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -16,13 +16,22 @@ class VigenereCipheringMachine {
     key = key.substr(0, message.length);
 
     let result = [];
+    let count = 0;
+
     for (let i = 0; i < message.length; i++) {
-      let char = message.charCodeAt(i) + (key.charCodeAt(i) - 65);
-      if (char > 90) {
-        char = 64 + ((key.charCodeAt(i) - 65) - (90 - message.charCodeAt(i)));
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        let k = count % key.length;
+        count++;
+        let char = message.charCodeAt(i) + key.charCodeAt(k) - 65;
+        if (char > 90 || char < 65) {
+          char = char - 26;
+        }
+        result.push(String.fromCharCode(char));
+      } else {
+        result.push(message[i]);
       }
-      result.push(String.fromCharCode(char));
     }
+
     return this.mode ? result.join('') : result.reverse().join('');
   }
 
@@ -31,7 +40,29 @@ class VigenereCipheringMachine {
 
     message = message.toUpperCase();
     key = key.toUpperCase();
+    while (key.length < message.length) {
+      key += key;
+    }
+    key = key.substr(0, message.length);
 
+    let result = [];
+    let count = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        let k = count % key.length;
+        count++;
+        let char = message.charCodeAt(i) - key.charCodeAt(k) + 65;
+        if (char < 65 || char > 90) {
+          char = char + 26;
+        }
+        result.push(String.fromCharCode(char));
+      } else {
+        result.push(message[i]);
+      }
+    }
+
+    return this.mode ? result.join('') : result.reverse().join('');
   }
 }
 
